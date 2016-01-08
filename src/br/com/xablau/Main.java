be494d3f3fd1c1ba8daebb2877c8bf424abfea94
cesarokada <cesaryokada@gmail.com>
2016-01-08 21:39:15 -0200

    second commit

---
+++ src/br/com/xablau/Main.java
@@ -0,0 +1,13 @@
+package br.com.xablau;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        initOS();
+    }
+
+    private static void initOS() {
+        System.out.println("I");
+    }
+}
